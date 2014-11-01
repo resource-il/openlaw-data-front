ad80dc9216fc2c174035d41a3e6b608f363d1d70
Yehuda Deutsch <yeh@uda.co.il>
2014-11-01 05:07:28 +0200

    Not yet ready, but on the way

--- js/openlaw.js
+++ js/openlaw.js
@@ -1,9 +1,23 @@
 angular.module('openLaw', [])
-    .controller('MyController', ['$scope', function ($scope) {
-        $scope.greetMe = 'Yehuda';
-    }])
+    .controller('selector', function ($scope) {
+        $scope.years = [];
+        startYear = 2005;
+        for (currentYear = startYear; currentYear <= new Date().getFullYear(); currentYear++) {
+            $scope.years.push(currentYear);
+        }
+        $scope.knessetList = [];
+        latestKnesset = 19;
+        for (currentKnesset = 1; currentKnesset <= latestKnesset; currentKnesset++) {
+            $scope.knessetList.push(currentKnesset);
+        }
+        $scope.select = function(event) {
+            if (event.preventDefault) event.preventDefault();
+            //url = this.href.substr(1);
+            //console.log(event.target.getAttribute('href'));
+        };
+    })
     .controller('bookletList', function ($scope, $http) {
-        $http.jsonp('http://law.resource.org.il/booklet/year/2014/?callback=JSON_CALLBACK').success(function (data) {
-            $scope.booklets = data.content;
-        });
+        //$http.jsonp('http://law.resource.org.il/booklet/knesset/12/?callback=JSON_CALLBACK').success(function (data) {
+        //    $scope.booklets = data.content;
+        //});
     });
