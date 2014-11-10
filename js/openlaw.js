fb97f587ae592fc1f14aa28d69503faef5eccee5
Yehuda Deutsch <yeh@uda.co.il>
2014-11-10 17:47:49 +0200

    Some changes

--- js/openlaw.js
+++ js/openlaw.js
@@ -11,15 +11,16 @@ var openLawApp = angular.module('openLaw', [])
             $scope.knessetList.push(currentKnesset);
         }
         //$scope.select = function (event) {
-            //if (event.preventDefault) event.preventDefault();
-            //url = this.href.substr(1);
-            //console.log(event.target.getAttribute('href'));
+        //    if (event.preventDefault) event.preventDefault();
+        //    url = this.href.substr(1);
+        //    console.log(event.target.getAttribute('href'));
         //};
     })
-    .controller('bookletList', function ($scope, $http) {
-        //$http.jsonp('http://law.resource.org.il/booklet/knesset/12/?callback=JSON_CALLBACK').success(function (data) {
-        //    $scope.booklets = data.content;
-        //});
+    .controller('bookletList', function ($scope, $http, $rootScope) {
+        console.log($rootScope);
+        $http.jsonp('http://law.resource.org.il/booklet/knesset/19/?callback=JSON_CALLBACK').success(function (data) {
+            $scope.booklets = data.content;
+        });
     });
 
 openLawApp.config(function ($locationProvider) {
@@ -32,6 +33,11 @@ openLawApp.config(function ($locationProvider) {
         $(".box").on("click", "header", function (event) {
             $this = $(this);
             $this.parents(".box").toggleClass('open');
+        });
+        $(".select-link").on("click", "", function (event) {
+            $this = $(this);
+            $(".select-link.selected").removeClass("selected");
+            $this.addClass("selected");
         });
     });
 })(jQuery);
